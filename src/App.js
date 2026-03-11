62a82065f2781f49e5ad8a73d71f42f8f1dab66b
osho20 <abhinaveosho2004@gmail.com>
2026-03-11 21:45:32 +0530

    Day 4: Deployed to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ export default function App(){
     }, 200);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/scan", {
+      const response = await fetch("https://phishguard-backend-xnb1.onrender.com/scan", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ url: url.trim() })
